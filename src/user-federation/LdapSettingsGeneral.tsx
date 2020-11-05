507ddaba42050c2f28b2f1f7f813aaec9f0b6656
Sarah Rambacher <srambach@redhat.com>
2020-11-05 14:14:48 -0500

    trying to do select controller

--- src/user-federation/LdapSettingsGeneral.tsx
+++ src/user-federation/LdapSettingsGeneral.tsx
@@ -1,15 +1,23 @@
-import { Form, FormGroup, Select, TextInput } from "@patternfly/react-core";
+import {
+  Form,
+  FormGroup,
+  Select,
+  SelectOption,
+  SelectVariant,
+  TextInput,
+} from "@patternfly/react-core";
 import { useTranslation } from "react-i18next";
-import React from "react";
+import React, { useState } from "react";
 import { HelpItem } from "../components/help-enabler/HelpItem";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { UserFederationLdapGeneralRepresentation } from "./models/user-federation";
 
 export const LdapSettingsGeneral = () => {
   const { t } = useTranslation("user-federation");
   const helpText = useTranslation("user-federation-help").t;
 
-  const { register, handleSubmit } = useForm<
+  const [open, isOpen] = useState(false);
+  const { register, handleSubmit, control } = useForm<
     UserFederationLdapGeneralRepresentation
   >();
   const onSubmit = (data: UserFederationLdapGeneralRepresentation) => {
@@ -53,20 +61,31 @@ export const LdapSettingsGeneral = () => {
           fieldId="kc-vendor"
           isRequired
         >
-          <Select
-            toggleId="kc-vendor"
-            // isOpen={openType}
-            onToggle={() => {}}
-            // variant={SelectVariant.single}
-            // value={selected}
-            // selections={selected}
-            // onSelect={(_, value) => {
-            //   setSelected(value as string);
-            //   setOpenType(false);
-            // }}
-            aria-label="Other"
-            isDisabled
-          ></Select>
+          <Controller
+            render={({ onChange, value }) => (
+              <Select
+                toggleId="kc-vendor"
+                onToggle={() => isOpen(!open)}
+                variant={SelectVariant.single}
+                selections={value}
+                onSelect={(_, value) => {
+                  onChange(value as string);
+                  isOpen(false);
+                }}
+                aria-label="Other"
+                // isDisabled
+              >
+                <SelectOption key={0} value="Choose..." isPlaceholder />
+                <SelectOption key={1} value="Mr" />
+                <SelectOption key={2} value="Miss" />
+                <SelectOption key={3} value="Mrs" />
+                <SelectOption key={4} value="Ms" />
+              </Select>
+            )}
+            control={control}
+            name="vendor"
+            defaultValue="1"
+          ></Controller>
         </FormGroup>
         <button type="submit">Test submit</button>
       </Form>
