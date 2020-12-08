01596f9bae20f91a78ba0d9799bec1144ccff9a3
Sarah Rambacher <srambach@redhat.com>
2020-12-08 16:00:49 -0500

    jumplinks on main content scroll but buggy

--- src/components/scroll-form/ScrollForm.tsx
+++ src/components/scroll-form/ScrollForm.tsx
@@ -5,7 +5,7 @@ import {
   GridItem,
   JumpLinks,
   JumpLinksItem,
-  Title,
+  PageSection,
 } from "@patternfly/react-core";
 
 import { FormPanel } from "./FormPanel";
@@ -16,29 +16,29 @@ type ScrollFormProps = {
   children: React.ReactNode;
 };
 
+// This must match the page id created in App.tsx unless another page section has been given hasScrollableContent
+const mainPageContentId = "#main-content-page-container";
+
 export const ScrollForm = ({ sections, children }: ScrollFormProps) => {
   const { t } = useTranslation("common");
-  const [active, setActive] = useState(sections[0]);
 
   const Nav = () => (
-    <div className={style.sticky}>
-      <Title headingLevel="h5" size="lg">
-        {t("jumpToSection")}
-      </Title>
-
-      <JumpLinks isVertical>
+    <PageSection className={style.sticky}>
+      <JumpLinks
+        isVertical
+        // scrollableSelector has to point to the id of the element whose scrollTop changes
+        // to scroll the entire main section, it has to be the pf-c-page__main
+        scrollableSelector={mainPageContentId}
+        label={t("jumpToSection")}
+      >
         {sections.map((cat) => (
-          <JumpLinksItem
-            isActive={active === cat}
-            key={cat}
-            href={`#${cat}`}
-            onClick={() => setActive(cat)}
-          >
+          // note that JumpLinks currently does not work with spaces in the href
+          <JumpLinksItem href={`#${cat.replace(/\s+/g, "-")}`}>
             {cat}
           </JumpLinksItem>
         ))}
       </JumpLinks>
-    </div>
+    </PageSection>
   );
 
   const nodes = Children.toArray(children);
@@ -46,7 +46,7 @@ export const ScrollForm = ({ sections, children }: ScrollFormProps) => {
     <Grid hasGutter>
       <GridItem span={8}>
         {sections.map((cat, index) => (
-          <FormPanel id={cat} key={cat} title={cat}>
+          <FormPanel id={cat.replace(/\s+/g, "-")} key={cat} title={cat}>
             {nodes[index]}
           </FormPanel>
         ))}
